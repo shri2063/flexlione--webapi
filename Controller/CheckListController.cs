9ca0838515c0614c51fb84a8813da252017b5871
shrikant@flexli.in <shrikant@flexli.in>
2022-01-07 19:04:13 +0530

    Checklist - view,add,delete feature added

---
+++ Controller/CheckListController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using m_sort_server.DataModels;
+using m_sort_server.EditModels;
+using m_sort_server.Services;
+using Microsoft.AspNetCore.Mvc;
+using m_sort_server.Models;
+using Microsoft.AspNetCore.Cors;
+
+
+namespace m_sort_server.Controller
+{
+    [Route("api/v1")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [EnableCors("AllowOrigin")]
+    [Produces("application/json")]
+    
+    public class TaskManagerController : ControllerBase
+    {
+        
+        [HttpGet("GetTaskList")]
+        [Consumes("application/json")]
+        
+        public List<TaskSheetEditModel> GetTaskList([FromQuery] string include,string taskId = null)
+        {
+            return TaskManagerService.GetTaskList(taskId,include);
+        }
+        
+        [HttpPut("CreateOrUpdateTask")]
+        [Consumes("application/json")]
+        
+        public TaskSheetEditModel CreateOrUpdateTask(TaskSheetEditModel task)
+        {
+            return TaskManagerService.CreateOrUpdateTask(task);
+        }
+        
+        [HttpDelete("DeleteTask")]
+        [Consumes("application/json")]
+        
+        public ActionResult<string> DeleteTask(string taskId)
+        {
+            TaskManagerService.DeleteTask(taskId);
+            return Ok();
+        }
+    }
+}
---
+++ DataModels/CheckList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace m_sort_server.DataModels
+{
+   
+    [Table("task_sheet")]
+    public class TaskSheet
+    {
+       [Key][Column("task_id")]
+        public string TaskId { get; set; }
+        
+        [Column("parent_task_id")]
+        public string ParentTaskId { get; set; }
+        
+        [Column("created_by")]
+        public string CreatedBy { get; set; }
+        
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+        
+        [Column("on_hold")]
+        public bool OnHold { get; set; }
+        
+        [Column("description")]
+        public string Description { get; set; }
+        
+        [Column("position_after")]
+        public string PositionAfter { get; set; }
+
+        
+       
+        
+       
+    }
+}
