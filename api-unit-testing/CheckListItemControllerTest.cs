c1afc86d624b33effd3fd936656e4b6f056e5b85
Tushar Garg <gargtushar739@gmail.com>
2022-06-27 16:36:37 +0530

    Appropriate changes made in CheckList unit tests

--- api-unit-testing/CheckListItemControllerTest.cs
+++ api-unit-testing/CheckListItemControllerTest.cs
@@ -1,42 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using m_sort_server;
 using m_sort_server.Controller;
 using m_sort_server.EditModels;
 using m_sort_server.Services;
+using m_sort_server.Utility;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace api_unit_testing
 {
     [Collection("Sequential")]
     
-    public class CheckListItemControllerTest
+    public class CheckListItemControllerTest : IDisposable
     {
+        //Runs before every test
         public CheckListItemControllerTest()
         {
             string connString = "Server=65.1.53.71;Port=5432;UserId=postgres;Password=3edc#EDC;Database=flexli-erp-alpha";
             ErpContext.SetConnectionString(connString);
+            Dispose();
+        }
+        
+        //Runs after every test
+        public void Dispose()
+        {
+            using (var db = new ErpContext())
+            {
+                db.CheckList.RemoveIfExists(CHECKLISTITEMID);
+                db.SaveChanges();
+                
+                // db.TaskDetail.RemoveIfExists(TASKID);
+                // db.SaveChanges();
+            }
         }
 
+        // private static TaskDetailEditModel _taskDetailEditModel = new TaskDetailEditModel()
+        // {
+        //     TaskId = "new-task",
+        //     CreatedBy = "14",
+        //     AssignedTo = "14",
+        //     Description = DESCRIPTION,
+        //     PositionAfter = "505"
+        //
+        // };
+        //
+        // private static TaskDetailEditModel
+        //     _taskDetail = TaskManagementService.CreateOrUpdateTask(_taskDetailEditModel);
+
+        private static string CHECKLISTITEMID = "unit-test-123";
+        private static string TASKID = "599";
         private const string DESCRIPTION = "unit-testing-description";
         private const string STATUS = "unit-testing-status";
         private const string COMMENT = "unit-testing-comment";
         private const string ATTACHEMENT = "unit-testing-attachment";
 
-        private static CheckListItemEditModel _checkListItem = new CheckListItemEditModel()
+        private const string NONEXISTANTTASKID = "non-existent-taskId";
+        private const string ITEMS = "items";
+        private const string NONITEMS = "any-string";
+
+        private const string NONITEMMESSAGE = "Error in finding required check list";
+
+        private const string INVALIDTASKIDMESSAGE =
+            "An error occurred while updating the entries. See the inner exception for details.";
+
+        private readonly CheckListItemEditModel _checkListItem = new CheckListItemEditModel()
         {
-            CheckListItemId = "",
-            TaskId = "8",
+            CheckListItemId = CHECKLISTITEMID,
+            TaskId = TASKID,
             Description = DESCRIPTION,
             Status = STATUS,
             Comment = COMMENT,
             Attachment = ATTACHEMENT
         };
         
-        private static CheckListItemEditModel _updateCheckListItem = new CheckListItemEditModel()
+        private readonly CheckListItemEditModel _invalidCheckListItem = new CheckListItemEditModel()
         {
-            CheckListItemId = "33",
-            TaskId = "8",
+            CheckListItemId = CHECKLISTITEMID,
+            TaskId = NONEXISTANTTASKID,
             Description = DESCRIPTION,
             Status = STATUS,
             Comment = COMMENT,
@@ -44,82 +87,114 @@ namespace api_unit_testing
         };
 
         [Fact]
-        private void givenInvalidTaskId_whenGetCheckListCalled_expectedNull()
+        private void givenInvalidTaskId_whenGetCheckListCalled_expectedNull() //read
         {
-            Assert.Empty(CheckListManagementService.GetCheckList("non-existent-taskId", "items"));
+            // Arrange
+
+            // Act
+            List<CheckListItemEditModel> checkList =
+                CheckListManagementService.GetCheckList(NONEXISTANTTASKID, ITEMS);
+            
+            // Assert
+            Assert.Empty(checkList);
         }
 
         [Fact]
-        private void givenExistingTaskIdAndIncludeContainItems_whenGetCheckListCalled_expectedNotNull()
+        private void givenExistingTaskIdAndIncludeContainItems_whenGetCheckListCalled_expectedNotNull() //read
         {
-            //CheckListManagementService.CreateOrUpdateCheckListItem(_checkListItem);
-            List<CheckListItemEditModel> checkList = CheckListManagementService.GetCheckList("8", "items");
+            // Arrange
+            CheckListItemEditModel checkListItem = CheckListManagementService.CreateOrUpdateCheckListItem(_checkListItem);
+
+            // Act
+            List<CheckListItemEditModel> checkList = CheckListManagementService.GetCheckList(TASKID, ITEMS);
+        
+            // Assert
             Assert.NotEmpty(checkList);
         }
-
+        
         [Fact]
-        private void givenExistingTaskIdAndIncludeNotContainsItems_whenGetCheckListCalled_expectedException()
+        private void givenExistingTaskIdAndIncludeNotContainsItems_whenGetCheckListCalled_expectedException() //read
         {
-            Assert.Throws<KeyNotFoundException>(() => CheckListManagementService.GetCheckList("8", "any-type"));
+            // Arrange
+            CheckListItemEditModel checkListItem = CheckListManagementService.CreateOrUpdateCheckListItem(_checkListItem);
+
+            // Act
+            
+            //Assert
+            var ex = Assert.Throws<KeyNotFoundException>(() => CheckListManagementService.GetCheckList(TASKID, NONITEMS));
+            Assert.Equal(NONITEMMESSAGE, ex.Message);
         }
-
+        
         [Fact]
-        private void whenCreatedCheckList_expectCorrectCreation()
+        private void whenCreatedOrUpdatedCheckList_expectCorrectCreationOrUpdation() //write
         {
+            // Arrange
+        
+            // Act
             CheckListItemEditModel checkListItem =
                 CheckListManagementService.CreateOrUpdateCheckListItem(_checkListItem);
-            Assert.NotNull(checkListItem);
-            
-            int flag = 0;
-            List<CheckListItemEditModel> itemList = CheckListManagementService.GetCheckList("8", "items");
-            itemList.ForEach(x =>
+            CHECKLISTITEMID = checkListItem.CheckListItemId;
+
+            CheckListItemEditModel updateEditModel = new CheckListItemEditModel()
             {
-                if (x.CheckListItemId == checkListItem.CheckListItemId)
-                {
-                    flag = 1;
-                }
-                    
-            });
-            
-            Assert.Equal(1, flag);
-
-            // List<CheckListItemEditModel> itemList = CheckListManagementService.GetCheckList("8", "items");
-            // Assert.Contains(checkListItem, itemList);
-        }
-
-        [Fact]
-        private void forExistingCheckListItem_whenUpdated_expectNoError()
-        {
+                CheckListItemId = CHECKLISTITEMID,
+                TaskId = TASKID,
+                Description = DESCRIPTION,
+                Status = STATUS,
+                Comment = COMMENT,
+                Attachment = ATTACHEMENT
+            };
             var temp = DateTime.Now.ToString("T");
-            _updateCheckListItem.Description = DESCRIPTION + temp;
-            CheckListItemEditModel updatedCheckListItem = CheckListManagementService.CreateOrUpdateCheckListItem(_updateCheckListItem);
-            Assert.NotNull(updatedCheckListItem);
+            updateEditModel.Description = DESCRIPTION + temp;
+            
+            CheckListItemEditModel updatedCheckListItem = CheckListManagementService.CreateOrUpdateCheckListItem(updateEditModel);
+            
+            // Assert
+            Assert.Equal(CHECKLISTITEMID, checkListItem.CheckListItemId);
 
             int flag = 0;
-            List<CheckListItemEditModel> itemList = CheckListManagementService.GetCheckList("8", "items");
+            List<CheckListItemEditModel> itemList = CheckListManagementService.GetCheckList(TASKID, ITEMS);
             itemList.ForEach(x =>
             {
-                if (x.Description == _updateCheckListItem.Description)
+                if (x.CheckListItemId == CHECKLISTITEMID)
                 {
                     flag = 1;
+                    Assert.Equal(TASKID, x.TaskId);
+                    Assert.Equal(updateEditModel.Description,x.Description);
+                    Assert.Equal(STATUS, x.Status);
+                    Assert.Equal(ATTACHEMENT, x.Attachment);
+                    Assert.Equal(COMMENT, x.Comment);
                 }
                     
             });
-            
-            Assert.Equal(1, flag);
+            Assert.Equal(1,flag);
         }
-
+        
         [Fact]
-        private void whenDeleteCheckListItemCalled_expectSuccessAndNoException()
+        private void whenDeleteCheckListItemCalled_expectSuccessAndNoException() //Delete
         {
-            CheckListManagementService.DeleteCheckListItem("non-existent-checklist-id");
+            // Arrange
             CheckListItemEditModel checkListItem =
                 CheckListManagementService.CreateOrUpdateCheckListItem(_checkListItem);
+            CHECKLISTITEMID = checkListItem.CheckListItemId;
+            
+            // Act
+            CheckListManagementService.DeleteCheckListItem("non-existent-checklist-id");
             CheckListManagementService.DeleteCheckListItem(checkListItem.CheckListItemId);
             
-            List<CheckListItemEditModel> itemList = CheckListManagementService.GetCheckList("8", "items");
-            itemList.ForEach(x=>{Assert.NotEqual(x.CheckListItemId, checkListItem.CheckListItemId);});
+            List<CheckListItemEditModel> itemList = CheckListManagementService.GetCheckList(TASKID, ITEMS);
             
+            // Assert
+            itemList.ForEach(x=>{Assert.NotEqual(CHECKLISTITEMID,x.CheckListItemId);});
+            
+        }
+        
+        [Fact]
+        private void whenCreatedCheckListWithInvalidTaskId_expectedException() //Relation
+        {
+            var ex = Assert.Throws<DbUpdateException>(() =>
+                CheckListManagementService.CreateOrUpdateCheckListItem(_invalidCheckListItem));
+            Assert.Equal(INVALIDTASKIDMESSAGE, ex.Message);
         }
     }
 }
