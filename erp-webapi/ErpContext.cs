8fd3913a19ea422736e1b1949860be96332b7190
Tushar Garg <gargtushar739@gmail.com>
2022-07-07 17:43:49 +0530

    RegisterTimeStampController

--- erp-webapi/ErpContext.cs
+++ erp-webapi/ErpContext.cs
@@ -29,6 +29,8 @@ namespace flexli_erp_webapi
         public DbSet<Template> Template { get; set; }
         
         public DbSet<TemplateTask> TemplateTask { get; set; }
+        
+        public DbSet<RegisterTimeStamp> RegisterTimeStamp { get; set; }
 
         // for logging sql queries
         private static readonly ILoggerFactory LoggerFactory  = new LoggerFactory().AddConsole((_,___) => true);
---
+++ erp-webapi/Models/DataModels/RegisterTimeStamp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace flexli_erp_webapi.DataModels
+{
+    [Table("exp_table")]
+    public class RegisterTimeStamp
+    {
+        [Key] [Column("stamp_id")]
+        public string StampId { get; set; }
+
+        [Column("stamp")]
+        public DateTime? Stamp { get; set; }
+        
+    }
+}
